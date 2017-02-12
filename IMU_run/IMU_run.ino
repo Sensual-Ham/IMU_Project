dccb162d8c39906f0cb0080c5a815850f9aa2c7c
Sensual-Ham <thegodfathergod@gmail.com>
2017-02-12 14:52:43 -0800

    Pitch and Roll Calculations
    Added pitch and roll calculations off of raw accelerometer data.

--- IMU_run/IMU_run.ino
+++ IMU_run/IMU_run.ino
@@ -1,10 +1,14 @@
 #include "IMU.h"
 #include "MatrixMath.h"
+#include <Math.h>
+#define RAD2DEG 57.2958
 
 float xAccel, yAccel, zAccel;
 float xGyro, yGyro, zGyro;
+double Pitch, Roll; 
 IMU myIMU;
 MatrixMath Matrix;
+
 
 void setup() {
   // put your setup code here, to run once:
@@ -14,29 +18,34 @@ void setup() {
 }
 
 void loop() {
-  // get & print accelerometer data
-  Serial.print("Accelerometer x: ");
-  xAccel = myIMU.accelRead(0);
-  Serial.println(xAccel);
-  Serial.print("Accelerometer y :");
-  yAccel = myIMU.accelRead(1);
-  Serial.println(yAccel);
-  Serial.print("Accelerometer z: ");
-  zAccel = myIMU.accelRead(2);
-  Serial.println(zAccel);
-
-  //Get & print gyro values
-  Serial.print("Gyro x: ");
+  // Get raw accelerometer and gyro data
+  // TODO: Implement filters for data
+  xAccel = myIMU.accelRead(0) / 1000;
+  yAccel = myIMU.accelRead(1) / 1000;
+  zAccel = myIMU.accelRead(2) / 1000;
   xGyro = myIMU.gyroRead(0);
-  Serial.println(xGyro);
-  Serial.print("Gyro y: ");
   yGyro = myIMU.gyroRead(1);
-  Serial.println(yGyro);
-  Serial.print("Gyro Z: ");
   zGyro = myIMU.gyroRead(2);
-  Serial.println(zGyro);
+
+  // Print raw data to serial 
+  Serial.print("Accelerometer x: ");  Serial.println(xAccel);
+  Serial.print("Accelerometer y :");  Serial.println(yAccel);
+  Serial.print("Accelerometer z: ");  Serial.println(zAccel);
+
+  Serial.print("Gyro x: "); Serial.println(xGyro);
+  Serial.print("Gyro y: "); Serial.println(yGyro);
+  Serial.print("Gyro Z: "); Serial.println(zGyro);
+
+  // Calculate pitch and roll from accelerometer data
+  // Roll: Around x-axis
+  // Pitch: Around y-axis
+  
+  Pitch = 2 * atan2(xAccel ,sqrt((xAccel*xAccel) + (zAccel*zAccel)) );
+  Roll = 2 * atan2(yAccel ,sqrt((yAccel*yAccel) + (zAccel*zAccel)) );
+  Serial.print("Pitch: "); Serial.println(Pitch * RAD2DEG); 
+  Serial.print("Roll: "); Serial.println(Roll * RAD2DEG);
   Serial.println( );
+  
   delay(1000);
 }
-
 
